24ba8a8a15bbe1010f54499a418e901afc1a3490
Luca Stefani <luca.stefani.ge1@gmail.com>
2017-03-05 20:26:49 +0100

    lithium: Add fingerprint
    Change-Id: If3b2527585ec32caddc457e1293f86fd27b6f1af

--- lineage.mk
+++ lineage.mk
@@ -4,8 +4,15 @@ $(call inherit-product, device/xiaomi/lithium/full_lithium.mk)
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 PRODUCT_NAME := lineage_lithium
-PRODUCT_BRAND := xiaomi
-PRODUCT_MANUFACTURER := xiaomi
 PRODUCT_DEVICE := lithium
+PRODUCT_BRAND := xiaomi
+PRODUCT_MODEL := Mi Mix
+PRODUCT_MANUFACTURER := xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE="lithium" \
+    PRODUCT_NAME="lithium" \
+    BUILD_FINGERPRINT="lithium-user 6.0.1 MXB48T 7.5.4 release-keys" \
+    PRIVATE_BUILD_DESC="Xiaomi/lithium/lithium:6.0.1/MXB48T/7.5.4:user/release-keys"
