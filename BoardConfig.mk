15a6265083945a96ad46cee24b328a65d3b5f0a4
Bruno Martins <bgcngm@gmail.com>
2017-12-16 22:09:09 +0000

    lithium: Add compatibility matrix
    Change-Id: Ibf6053f1a7e9df5384664379772cb8757e897622

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -29,6 +29,7 @@ AUDIO_FEATURE_ELLIPTIC_ULTRASOUND_SUPPORT := true
 
 # HIDL
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
+DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
 
 # Kernel
 TARGET_KERNEL_CONFIG := lithium_defconfig
