d0037b68bbaa343c47acbeba4e2a87f1870bd55b
Bruno Martins <bgcngm@gmail.com>
2018-02-01 22:04:45 +0100

    lithium: Set matching build fingerprint for system, vendor and boot images
    Change-Id: I555c15eddc6dff694285ab1b23f6f9adfd64e535

--- lineage.mk
+++ lineage.mk
@@ -14,5 +14,7 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE="lithium" \
     PRODUCT_NAME="lithium" \
-    BUILD_FINGERPRINT="Xiaomi/lithium/lithium:6.0.1/MXB48T/V8.2.4.0.MAHMIDL:user/release-keys" \
     PRIVATE_BUILD_DESC="lithium-user 6.0.1 MXB48T V8.2.4.0.MAHMIDL release-keys"
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := "Xiaomi/lithium/lithium:6.0.1/MXB48T/V8.2.4.0.MAHMIDL:user/release-keys"
