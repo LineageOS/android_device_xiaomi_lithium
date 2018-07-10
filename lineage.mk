73bd16a97a90be0dcceebeaf00097d4bc86dd06e
Bruno Martins <bgcngm@gmail.com>
2018-07-10 18:59:19 +0100

    lithium: Update build fingerprint from Oreo-based MIUI 9 Global Stable
     * Newly released, just few days after the last build fingerprint update
    
    Change-Id: I6b472fa7843464f6f7f7f36333ad2216df41cf10

--- lineage.mk
+++ lineage.mk
@@ -14,7 +14,7 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE="lithium" \
     PRODUCT_NAME="lithium" \
-    PRIVATE_BUILD_DESC="lithium-user 7.0 NRD90M V9.5.5.0.NAAMIFA release-keys"
+    PRIVATE_BUILD_DESC="lithium-user 8.0.0 OPR1.170623.032 V9.6.1.0.OAHMIFD release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "Xiaomi/lithium/lithium:7.0/NRD90M/V9.5.5.0.NAAMIFA:user/release-keys"
+BUILD_FINGERPRINT := "Xiaomi/lithium/lithium:8.0.0/OPR1.170623.032/V9.6.1.0.OAHMIFD:user/release-keys"
